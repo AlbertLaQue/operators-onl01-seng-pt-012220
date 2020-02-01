75b2a7f4a3978ecbc631b6d1e262ca28c43d76b3
Albert LaQue <albertlaque@gmail.com>
2020-02-01 01:30:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,10 +2,10 @@
 # Will use if/else to build
 # true if below 40 or above 60
 def unsafe?(speed)
-  if speed < 40 || speed > 60
-    "false"
-  else 
+  if speed < 40 && speed > 60
     "true"
+  else 
+    "false"
   end
 end 
 
